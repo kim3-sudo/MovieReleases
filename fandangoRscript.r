5fc4eceb5f2b3bf71d3f6a5d13c3cf4a66b91f81
Sejin Kim <kim3@kenyon.edu>
2019-10-09 14:00:27 -0400

    Updated MovieReleases.Rproj and fandangoRscript.r

--- fandangoRscript.r
+++ fandangoRscript.r
@@ -11,6 +11,8 @@ library(mosaic)
 ## LOAD DATA
 #SEJIN'S FILE IMPORT
 fandango <- read.csv("C:/Users/skim2/Downloads/fandango.csv")
+fandango <- read.csv("C:/Users/kim3/Documents/R/MovieReleases/fandango.csv")
+View(fandango)
 
 # SHANTI'S FILE IMPORT
 fandango <- read.csv("")
@@ -21,3 +23,11 @@ fandango <- read.csv("")
 
 ############################################################
 ##### R CODE #####
+
+# EDA
+plot(MONTH~RT_user_norm, data = fandango)
+plot(RT_user_norm, data = filter(fandango, MONTH=="MARCH", YEAR=="2014",fandango))
+fandango$YEAR
+rtuserbydumpmod <- lm(RT_user_norm~DUMPMONTH_BIN, data = fandango)
+rtuserbydumpmod
+summary(rtuserbydumpmod)
